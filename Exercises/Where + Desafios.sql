64619b7c1ed9e370bc19e5fe9336364ee29a9792
Iago América <iago6489@gmail.com>
2021-11-02 20:12:22 -0300

    Update Where + Desafios.sql

--- Exercises/Where + Desafios.sql
+++ Exercises/Where + Desafios.sql
@@ -1,19 +1,15 @@
 /*Desafio 1 -> A equipe de produção de produtos precisa do nome de todas as peças que pesam
-naus de 500kg mas não mais que 700kg para inspeção*/
-
-/*Desafio 2 -> Foi pedido pela equipe de marketing uma relação de todos os empregados que são casados e são assalariados*/
-
-/*Desafio 3 -> Um usuário chamado Peter Krebs está devendo um pagamento, consiga o email dele para que possamos enviar uma cobrança*/
-
-
+mais de 500kg mas não mais que 700kg para inspeção*/
 SELECT Name
 FROM Production.Product
-WHERE Weight > 500 and Weight < 700
+WHERE Weight > 500 and Weight <= 700
 
+/*Desafio 2 -> Foi pedido pela equipe de marketing uma relação de todos os empregados que são casados e são assalariados*/
 SELECT*
 FROM HumanResources.Employee
 WHERE MaritalStatus = 'm' and SalariedFlag = 1
 
+/*Desafio 3 -> Um usuário chamado Peter Krebs está devendo um pagamento, consiga o email dele para que possamos enviar uma cobrança*/
 SELECT*
 FROM Person.Person
 WHERE FirstName = 'peter' and LastName = 'krebs'

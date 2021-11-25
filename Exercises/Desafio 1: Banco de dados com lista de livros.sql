02b09095e81f0ee7af164cab8a77f7d7a9ab9dd0
Iago América <90301260+Iago-America@users.noreply.github.com>
2021-11-25 01:13:46 -0300

    Delete Desafio 1: Banco de dados com lista de livros.sql

--- Exercises/Desafio 1: Banco de dados com lista de livros.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE books (id INTEGER PRIMARY KEY, name TEXT, rating NUMERIC);
-
-INSERT INTO books VALUES (1,"Clube de luta",5);
-INSERT INTO books VALUES (2,"Senhor dos Anéis",8);
-INSERT INTO books values (3,"Alienista",6.5);
-
-SELECT* FROM books

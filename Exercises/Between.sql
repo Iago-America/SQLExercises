b5e42a0b852a61a7bb034f07a1291219b757d39d
Iago América <iago6489@gmail.com>
2021-11-25 08:11:16 -0300

    Create Between.sql

---
+++ Exercises/Between.sql
@@ -0,0 +1,6 @@
+-- Between
+/*O Between é usado para encontrar o valor entre um valor mínimo e o valor máximo*/
+
+SELECT *
+FROM HumanResources.Employee
+WHERE HireDate BETWEEN '2009/01/01' and '2010/01/01'
